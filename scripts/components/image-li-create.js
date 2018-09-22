3582240a9f4ca1e72e28bc46608e0e1bfc0b6dcf
Lucid <29291906+IndecisiveBoolean@users.noreply.github.com>
2018-09-22 19:33:53 -0400

    removed unneeded files for overhaul.

--- scripts/components/image-li-create.js
+++
@@ -1,8 +0,0 @@
-Vue.component("image-li-create", {
-  props: ["content"],
-  template: `
-    <project-create>
-      <li><img class="tech-img" :src="content.technologiesImages[0]"></li>
-    </project-create>
-  `,
-});
